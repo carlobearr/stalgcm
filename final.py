fc7812581dae045b26c7bbb799acd5597144b16c
carlobearr <carlo_luis_v_santos@dlsu.edu.ph>
2020-04-28 16:01:11 +0800

    finished

--- final.py
+++ final.py
@@ -1,32 +1,24 @@
 from sys import stdin
+
 def error():
     print("NO")
     exit()
 
-symbol = "symbol"
-tag = "tag"
-attribute = "attribute"
-operator = "operator"
-attrvalue = "attribute value"
-value = "value"
-
 xml = ""
 result = []
 
-'''
 while True:
     try:
         a = input()
         xml = xml + a
     except EOFError:
         break
-
 '''
 while True:
     a = input()
     if a == "":
         break
-    xml = xml + a
+    xml = xml + a'''
 
 status = 0
 token = 0
@@ -36,13 +28,14 @@ temp = ""
 for i in range(len(xml)):
     if status == 0:
         if xml[i] == "<":
-            result.append(["symbol", "<"])
+            result.append(["Symbol", "<"])
             token = token + 1
             status = 1
 
     elif status == 1:
         if (xml[i] == "?" or xml[i] == "/"):
-            result.append(["symbol", xml[i]])
+            temp = temp + xml[i]
+            result.append(["Symbol", temp])
             token = token + 1
             temp = ""
             status = 2
@@ -55,7 +48,7 @@ for i in range(len(xml)):
             temp = temp + xml[i]
             status = 3
         elif (xml[i] == ">"):
-            result.append(["symbol", xml[i]])
+            result.append(["Symbol", xml[i]])
             token = token + 1
             status = 9
     
@@ -65,16 +58,16 @@ for i in range(len(xml)):
         elif (xml[i] == ':' or xml[i] == '-' or xml[i] == '.'):
             error()
         elif (xml[i] == " "):
-            result.append(["tag", temp])
+            result.append(["Tag", temp])
             temp = ""
             token = token + 1
             status = 4
         elif (xml[i] == ">"):
-            result.append(["tag", temp])
+            result.append(["Tag", temp])
             temp = ""
             token = token + 1
             
-            result.append(["symbol", ">"])
+            result.append(["Symbol", ">"])
             token = token + 1
             status = 9
 
@@ -84,7 +77,7 @@ for i in range(len(xml)):
             status = 5
         elif (xml[i] == "?" or xml[i] == "/"):
             temp = temp + xml[i]
-            result.append(["symbol", temp])
+            result.append(["Symbol", temp])
             temp = ""
             token = token + 1
             status = 2
@@ -93,12 +86,12 @@ for i in range(len(xml)):
         if (xml[i].isalpha() or xml[i].isdigit() or xml[i] == "_"):
             temp = temp + xml[i]
         elif (xml[i] == "="):
-            result.append(["attribute", temp])
+            result.append(["Attribute", temp])
             token = token + 1
             temp = ""
             status = 6
 
-            result.append(["operator", xml[i]])
+            result.append(["Operator", xml[i]])
             token = token + 1
     elif status == 6:
         if (xml[i] == '\"'):
@@ -109,23 +102,22 @@ for i in range(len(xml)):
             temp = temp + xml[i]
         else:
             temp = temp + xml[i]
-            result.append(["attribute value", temp])
+            result.append(["Attribute Value", temp])
             temp = ""
             token = token + 1
             status = 8
     elif status == 8:
         if (xml[i] == ">"):
-            result.append(["symbol", xml[i]])
+            result.append(["Symbol", xml[i]])
             token = token + 1
             status = 9
         elif (xml[i] == " "):
             status = 4
         elif (xml[i] == "?"):
-            result.append(["symbol", xml[i]])
+            result.append(["Symbol", xml[i]])
     elif status == 9:
         if (xml[i] == "<"):
-            print("A")
-            result.append(["symbol", "<"])
+            result.append(["Symbol", "<"])
             token = token + 1
             status = 1
         elif (xml[i] == " " or xml[i] == "\n"):
@@ -135,24 +127,26 @@ for i in range(len(xml)):
             temp = temp + xml[i]
     elif status == 10:
         if (xml[i] == "<"):
-            result.append(["value", "<"])
+            result.append(["Value", temp])
             token = token + 1
             temp = ""
 
-            result.append(["symbol", "<"])
+            result.append(["Symbol", "<"])
             token = token + 1
             status = 1
         else:
             temp = temp + xml[i]
 
 #print(result)
-for i in range(len(result)):
-    print(result[i])
+'''for i in range(len(result)):
+    print(result[i])'''
 status = 0
 count = 0
 store = []
-
-while (token != 0):
+store.append("#")
+'''print("==================")'''
+while (token != count):
+    #print(store)
     if status == 0:
         if (result[count][1] == "<"):
             status = 1
@@ -172,7 +166,7 @@ while (token != 0):
         else:
             error()
     elif status == 3:
-        if (result[count][0] == "attribute"):
+        if (result[count][0] == "Attribute"):
             status = 4
             count = count + 1
         elif (result[count][1] == "?"):
@@ -187,7 +181,7 @@ while (token != 0):
         else:
             error()
     elif status == 5:
-        if (result[count][0] == "attribute value"):
+        if (result[count][0] == "Attribute Value"):
             status = 3
             count = count + 1
         else:
@@ -202,13 +196,13 @@ while (token != 0):
         if (result[count][1] == "<"):
             status = 8
             count = count + 1
-        elif (result[count][0] == "value"):
+        elif (result[count][0] == "Value"):
             status = 12
             count = count + 1
         else:
             error()
     elif status == 8:
-        if (result[count][0] == "tag"):
+        if (result[count][0] == "Tag"):
             status = 9
             count = count + 1
             store.append(result[count-1][1])
@@ -221,13 +215,13 @@ while (token != 0):
         if (result[count][1] == ">"):
             status = 7
             count = count + 1
-        elif (result[count][0] == "attribute"):
+        elif (result[count][0] == "Attribute"):
             status = 13
             count = count + 1
         else:
             error()
     elif status == 10:
-        if (result[count][0] == "tag" and store[len(store)-1] == result[count][1]):
+        if (result[count][0] == "Tag" and (store[len(store)-1] == result[count][1])):
             status = 11
             store.pop()
             count = count +1
@@ -251,7 +245,7 @@ while (token != 0):
         else:
             error()
     elif status == 14:
-        if (result[count][0] == "attribute value"):
+        if (result[count][0] == "Attribute Value"):
             status = 15
             count = count + 1
         else:
@@ -260,7 +254,7 @@ while (token != 0):
         if (result[count][1] == "/"):
             status = 11
             count = count + 1
-        elif (result[count][0] == "attribute"):
+        elif (result[count][0] == "Attribute"):
             status = 13
             count = count + 1
         elif (result[count][1] == ">"):
@@ -268,5 +262,6 @@ while (token != 0):
             count = count + 1
         else:
             error()
+    #token = token - 1
 
 print("YES")
